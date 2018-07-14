49e5a5ae4b706110c5722bc1fb812746ec62fdde
ISTC-CREATE LAB <edgarvardanyan1999@gmail.com>
2018-07-14 15:23:47 +0400

    k-nn

--- k-NN/k_nn.py
+++ k-NN/k_nn.py
@@ -1,4 +1,7 @@
-class K_NN:
+import numpy as np
+from collections import Counter
+
+class K_NN(object):
     def __init__(self, k):
         """
         :param k: number of nearest neighbours
@@ -9,7 +12,7 @@ class K_NN:
         """
         :param data: 3D array, where data[i, j] is i-th classes j-th point (vector: D dimenstions)
         """
-        # TODO: preprocessing
+        self.data = np.array(data)
         pass
 
     def predict(self, data):
@@ -18,9 +21,21 @@ class K_NN:
         :return: array of integers
         """
         data = np.array(data)
-        shp = data.shape
-        if len(data.shape) == 1:
-            data = data.reshape([1] + list(data.shape))
-        # TODO: predict
-        prediction = np.array([0])
-        return prediction.reshape(shp[:-1])
+        distances = np.empty((data.shape[0], self.data.shape[0] *  self.data.shape[1])).astype(object)
+        for k in range(data.shape[0]):
+            for i in range(self.data.shape[0]):
+                for j in range(self.data.shape[1]):
+                    distances[k][i*self.data.shape[1] + j] = (self.distance(data[k], self.data[i][j]), i)
+        distances.sort(axis = 1)
+        a = np.empty((data.shape[0], self.k)).astype(object)
+        classes = np.empty(data.shape[0]).astype(object)
+        for i in range(data.shape[0]):
+            for j in range(self.k):
+                a[i][j] = distances[i][j][1]
+            dict1 = Counter(a[i])
+            classes[i] = max(dict1, key = dict1.get)
+            
+        return classes
+
+    def distance(self, a, b):
+        return np.sum((a-b)**2)**0.5
